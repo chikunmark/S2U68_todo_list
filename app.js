f109ef139193fb25861d4db01088b767727a2cc9
chikunmark <chikunmark@gmail.com>
2022-12-13 16:42:44 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
-const app = express()
 const port = 3000
+
+const mongoose = require('mongoose')
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!!')
+})
+
+db.once('open', () => {  // 因只會發生一次，所以用 once
+  console.log('mongoDB connected!!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world!')
