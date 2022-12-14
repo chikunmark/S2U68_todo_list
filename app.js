720541b8be38bd5a30cab5a30f7d41aab87cb34d
chikunmark <chikunmark@gmail.com>
2022-12-14 16:38:13 +0800

    modify index.hbs

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const todo = require('./models/todo')
+
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 const db = mongoose.connection
@@ -25,8 +27,14 @@ db.once('open', () => {  // 因只會發生一次，所以用 once
 })
 
 app.get('/', (req, res) => {
-  // res.send('hello world!')
-  res.render('index')
+  // res.send('hello world!') // 引入 handlebars 後就不用了
+
+  // 拿到所有 Todo 資料
+  todo.find()  // find 的 () 內也能傳參數做處理
+    .lean() // mongoose 做的都不要，我們只要單純地 JS 物件
+    // .then(todos => res.render('index', {todos: todos})) // 然後，整理後會有個 todos 資料陣列，把它 render 進 index.hbs 裡
+    .then(todos => res.render('index', { todos })) // {todos} 是上面的簡寫，僅在 key/value 一樣時能用 (至少我現在不想用，因為會看不懂資料從哪對應到哪)
+    .catch(error => console.error(error)) // 若有錯，把它印出
 })
 
 app.listen(port, () => {
